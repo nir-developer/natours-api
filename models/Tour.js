7f0889b946712e33775fc549531dc301aedbc0cd
nir-developer <nir100.developer@gmail.com>
2024-04-29 12:32:38 +0300

    108:Data-Validation-Build-in-Mongoose

--- models/Tour.js
+++ models/Tour.js
@@ -9,7 +9,9 @@ const tourSchema = new mongoose.Schema({
         type:String, 
         required:[true, 'A tour must have a name'], 
         unique:true, 
-        trim:true
+        trim:true, 
+        maxlength: [40, 'A tour name must have less or equal 40 characters'], 
+        minlength: [10, 'A tour name must have more or equal 10 characters']
     }, 
     //NOTE - I MUST HAVE THIS PROPERTY DEFINED HERE IN THE SCHEMA! 
     //OTHERWISE THE CODE IN THE PRE-SAVE M.W : this.slug = slugify(this.name...)
@@ -25,11 +27,20 @@ const tourSchema = new mongoose.Schema({
     }, 
     difficulty:{
         type:String, 
-        required:[true, 'A tour must have a difficulty']
+        required:[true, 'A tour must have a difficulty'],
+        //IMPORTANT ENUM ADDING THE ERROR MESSAGE TO ENUM - Create an extra object:
+        enum:{
+            values:['easy', 'medium', 'difficult'], 
+            message:'Difficulty is either: easy, medium, difficult'
+        }
     }, 
     ratingsAverage:{
-        type:Number, 
+        type:Number,
         default:4.5 ,
+        //min-max :;ON NUMBERS AND DATES!
+        min:[1,'Rating must be above 1.0'],
+        max:[5,'Rating must be below 5.0']
+
     }, 
     ratingsQuantity:{
         type:Number, 
