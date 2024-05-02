41783b61aa25ba8e926c4b55dc228d854fff1c6d
nir-developer <nir100.developer@gmail.com>
2024-05-02 14:10:12 +0300

    10:129 Singning Up Users

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,23 +3,37 @@ const User = require('../models/User')
 const catchAsync = require('../utils/catchAsync')
 
 
+const jwt = require('jsonwebtoken')
+
+
 exports.signup = catchAsync(async (req,res,next) =>{
 
-
+    //DONT READ OTHER REQUEST FIELDS COMMING FROM THE USER!!(like roles etc..)
     const newUser = await User.create({
         
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            passwordConfirm: req.body.passwordConfirm
-        }
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    }
     ); 
 
+
+    const token =  jwt.sign
+        (
+         {
+          id:newUser._id},
+          process.env.JWT_SECRET,
+         {expiresIn:process.env.JWT_EXPIRES_IN}
+        )
+
+    console.log(token)
     
     console.log(newUser); 
 
     res.status(201).json({
         status:'success', 
+        token,
         data:{
             user:newUser
         }
